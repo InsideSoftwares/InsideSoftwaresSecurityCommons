7f4bb11c2d34a9f55b307b16d903e584c8ad840a
Samuel Cunha <samukasaww@live.com>
2023-09-15 13:13:17 -0300

    Melhoria na obteção do usuario logado.

--- security/src/main/java/br/com/insidesoftwares/utils/InsideSoftwaresUserAuthenticationBean.java
+++ security/src/main/java/br/com/insidesoftwares/utils/InsideSoftwaresUserAuthenticationBean.java
@@ -1,13 +1,26 @@
 package br.com.insidesoftwares.utils;
 
 import br.com.insidesoftwares.commons.specification.InsideSoftwaresUserAuthentication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
+@Slf4j
 public class InsideSoftwaresUserAuthenticationBean implements InsideSoftwaresUserAuthentication {
+
+    private static final String NO_LOGGED_USER = "Non-logged-in user";
+
     @Override
     public String findUserAuthentication() {
-        return SecurityContextHolder.getContext().getAuthentication().getName();
+        log.debug("Find User Authentication");
+
+        if(Objects.isNull(SecurityContextHolder.getContext())) return NO_LOGGED_USER;
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Objects.nonNull(authentication) ? authentication.getName() : NO_LOGGED_USER;
     }
 }
